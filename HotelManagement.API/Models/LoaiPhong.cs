eb48d9fb9aaf16d23a1398734b2b48b1e5809ce3
fugdin <fugdin@gmail.com>
2025-03-31 19:10:47 +0700

    retard

--- HotelManagement.API/Models/LoaiPhong.cs
+++
@@ -1,9 +0,0 @@
-namespace HotelManagement.API.Models;
-
-public class LoaiPhong
-{
-    public string MaLoaiPhong { get; set; } = string.Empty;
-    public string TenLoai { get; set; } = string.Empty;
-    public string MoTa { get; set; } = string.Empty;
-    public decimal GiaCoBan { get; set; }
-}
